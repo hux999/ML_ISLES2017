461fc4021c783f7ea4cd84b3175d7f1f8520015c
Alduin Hu <aldhu@malong.com>
2018-02-19 14:40:23 +0800

    add BRATS dataset

--- dataset.py
+++ dataset.py
@@ -2,14 +2,15 @@ import os
 import sys
 import random
 
-import nibabel as nib 
+import nibabel as nib
+import SimpleITK as sitk
 import cv2
 import numpy as np
 
 import torch
 from torch.utils.data import Dataset 
 
-def LoadOnePerson(data_root):
+def LoadOnePersonNii(data_root):
     folders = os.listdir(data_root)
     data = {}
     for folder in folders:
@@ -17,6 +18,16 @@ def LoadOnePerson(data_root):
         nii_file = os.path.join(data_root, folder, folder+'.nii')
         nii_data = nib.load(nii_file)
         data[data_type] = nii_data.get_data().squeeze()
+    return data
+
+def LoadOnePersonMha(data_root):
+    folders = os.listdir(data_root)
+    data = {}
+    for folder in folders:
+        data_type = folder.split('.')[4]
+        mha_file = os.path.join(data_root, folder, folder+'.mha')
+        mha_data = sitk.ReadImage(mha_file)
+        data[data_type] = sitk.GetArrayFromImage(mha_data).transpose([1,2,0])
     return data
 
 def StackData(person_data):
@@ -30,12 +41,18 @@ def StackData(person_data):
             if len(img_data.shape) == 3:
                 h,w,d = img_data.shape
                 img_data.shape = [h,w,d,1]
+                channels.append(img_data.astype(np.float32))
             else:
-                img_data = np.mean(img_data.astype(np.float32), axis=3)
-                h,w,d = img_data.shape
-                img_data.shape = [h,w,d,1]
-            #print(img_data.shape)
-            channels.append(img_data.astype(np.float32))
+                h,w,d,_ = img_data.shape
+                avg_img_data = np.mean(img_data.astype(np.float32), axis=3)
+                avg_img_data.shape = [h,w,d,1]
+                channels.append(avg_img_data.astype(np.float32))
+                max_img_data = np.max(img_data.astype(np.float32), axis=3)
+                max_img_data.shape = [h,w,d,1]
+                channels.append(max_img_data.astype(np.float32))
+                min_img_data = np.min(img_data.astype(np.float32), axis=3)
+                min_img_data.shape = [h,w,d,1]
+                channels.append(min_img_data.astype(np.float32))
     channels = np.concatenate(channels, axis=3)
     return channels, label
 
@@ -89,22 +106,50 @@ def MakeGrid(imgs, width=8):
     return concat_img
 
 def Visualize(person_data):
-    normlize_data = {}
+    color_bar = [
+            (0, 0, 0),
+            (0, 255, 0),
+            (0, 0, 255),
+            (255, 0, 0),
+            (255, 0, 255),
+            (0, 255, 255),
+            (255, 255, 0)
+            ]
+    # normalize each channel in range [0, 255]
+    normalize_data = {}
     for img_type, img_data in person_data.items():
-        data = img_data.astype(np.float32)
-        data *= 255.0/img_data.max()
-        normlize_data[img_type] = data.astype(np.uint8)
-    for i in range(19):
-        groundtruth = 'OT' in normlize_data.keys()
+        if img_type == 'OT':
+            print(np.max(img_data), np.min(img_data))
+            max_label = np.max(img_data)
+            data = img_data
+        else:
+            data = img_data.astype(np.float32)
+            data *= 255.0/img_data.max()
+        normalize_data[img_type] = data.astype(np.uint8)
+    # for each time slice
+    for i in range(normalize_data.values()[0].shape[2]):
+        groundtruth = 'OT' in normalize_data.keys()
+        # parse groundtruth
         if groundtruth:
-            gt = (normlize_data['OT'][:,:, i]>128).astype(np.uint8)
+            ot_data = normalize_data['OT'][:,:, i] 
+            gt = (ot_data>0).astype(np.uint8) # TODO assume 0 is background class
             contours,_ = cv2.findContours(gt.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            R = np.zeros(ot_data.shape, np.uint8)
+            G = np.zeros(ot_data.shape, np.uint8)
+            B = np.zeros(ot_data.shape, np.uint8)
+            print(np.unique(ot_data))
+            for label in range(1, max_label+1):
+                R[ot_data==label] = color_bar[label][0]
+                G[ot_data==label] = color_bar[label][1]
+                B[ot_data==label] = color_bar[label][2]
+            ot_data = cv2.merge([B, G, R])
+            cv2.putText(ot_data, 'OT', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255))
         imgs = {}
-        for img_type, img_data in normlize_data.items():
+        for img_type, img_data in normalize_data.items():
             if len(img_data.shape) == 3:
                 imgs[img_type] = img_data[:, :, i]
             else:
-                continue
+                continue #TODO
                 for j in range(img_data.shape[3]):
                     imgs[img_type+('_%d'%j)] = img_data[:, :, i, j]
         for img_type, img_data in imgs.items():
@@ -113,17 +158,16 @@ def Visualize(person_data):
                 cv2.drawContours(img_data, contours, -1, (255,0,0), 1)
             cv2.putText(img_data, img_type, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255))
             imgs[img_type] = img_data
+        if groundtruth and len(contours)>0:
+            imgs['OT'] = ot_data
         print(len(imgs))
         concat_img = MakeGrid(imgs.values(), 8)
         cv2.imshow('img', concat_img)
         cv2.waitKey()
 
-class ISLESDataset(Dataset):
+class ScanDataset(Dataset):
     def __init__(self, folders, sample_shape=(96,96,5), means=None, norm=None, is_train=False):
-        data_list = [LoadOnePerson(folder) for folder in folders]
-        data_list = [StackData(data) for data in data_list]
-        label_list = [data[1] for data in data_list]
-        data_list = [data[0] for data in data_list]
+        data_list, label_list = self.load_data(folders)
         data_list,means,norm = Normalize(data_list, means, norm)
         self.folders = folders
         self.label_list = label_list
@@ -153,9 +197,31 @@ class ISLESDataset(Dataset):
     def eval(self):
         self.is_train = False
 
+class ISLESDataset(ScanDataset):
+    def __init__(self, folders, sample_shape=(96,96,5), means=None, norm=None, is_train=False):
+        super(ISLESDataset, self).__init__(folders, sample_shape, means, norm, is_train)
+
+    def load_data(self, folders):
+        data_list = [LoadOnePersonNii(folder) for folder in folders]
+        data_list = [StackData(data) for data in data_list]
+        label_list = [data[1] for data in data_list]
+        data_list = [data[0] for data in data_list]
+        return data_list, label_list
+
+class BRATSDataset(ScanDataset):
+    def __init__(self, folders, sample_shape=(96,96,5), means=None, norm=None, is_train=False):
+        super(BRASTDataset, self).__init__(folders, sample_shape, means, norm, is_train)
+
+    def load_data(self, folders):
+        data_list = [LoadOnePersonMha(folder) for folder in folders]
+        data_list = [StackData(data) for data in data_list]
+        label_list = [data[1] for data in data_list]
+        data_list = [data[0] for data in data_list]
+        return data_list, label_list
+
 def test_visulize():
     root = sys.argv[1]
-    data = LoadOnePerson(root)
+    data = LoadOnePersonMha(root)
     for img_type, img_data in data.items():
         print(img_type, img_data.shape)
     Visualize(data)
